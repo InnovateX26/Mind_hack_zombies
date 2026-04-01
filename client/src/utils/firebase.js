a5dcb89cf1dacc05c40990f0d14799e04daa0c31
amitxyz12 <amitxyz502@gmail.com>
2026-04-01 11:15:19 +0000

    Finalize tailwind css integration and update frontend styles

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -2,9 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey:"AIzaSyAiL6soTy0x4_IT54IQwkbYSPo-5GMrFlI",
-  authDomain: "ai-interview-simulator-af8b4.firebaseapp.com",
-  projectId:"ai-interview-simulator-af8b4",
+  apiKey: "AIzaSyAcYozN2iRUlaxVF6KKLo2niPiVlFnrZ18",
+  authDomain: "ai-interview-project-d7343.firebaseapp.com",
+  projectId: "ai-interview-project-d7343",
+  storageBucket: "ai-interview-project-d7343.firebasestorage.app",
+  messagingSenderId: "72423366327",
+  appId: "1:72423366327:web:569435ca7528d74af43732",
+  measurementId: "G-RYN31QFPRL"
 };
 
 const app = initializeApp(firebaseConfig);
